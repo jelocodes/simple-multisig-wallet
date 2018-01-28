d65a3ae2d2277bec72e60fcc1401a6460a72090c
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-01-28 13:25:59 -0500

    finish permissions functions

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -11,6 +11,34 @@ contract SimpleWallet {
 
 	// constructor function
 	function simpleWallet() {
-		owner = msg.sender; //setting the owner as the wallet creator (good practice in order to avoid mishaps like outsiders deleting the contract)
+		owner = msg.sender; //setting the owner as the wallet creator so we know who created the contract (good practice in order to avoid mishaps like outsiders claiming ownership and doing something like deleting the contract)
 	}
+
+	// here we add new validated addresses to our mapping. This can once again only be called by the owner.
+	function allowAddressToSendMoney(address _address) {
+		if (msg.sender == owner) {
+			isAllowedToSendFundsMapping[_address] = true;
+		}
+	}
+
+	//owner giveth, owner taketh away. pretty self explanatory.
+	function disallowAddressToSendMoney(address _address) {
+		if (msg.sender == owner) {
+			isAllowedToSendFundsMapping[_address] = false;
+		}
+	}
+
+	// a reader function to check if an address is allowed wallet access
+	function isAllowedToSend(address _address) constant returns (bool) {
+		return isAllowedToSendFundsMapping[_address] || msg.sender == owner;
+	}
+
+	// self-destruct function that the owner can call which destroys the contract and deposits all remaining funds to the owner's wallet address
+	function killWallet() {
+		if(msg.sender == owner) {
+			suicide(owner);
+		}
+	}
+
+
 }
