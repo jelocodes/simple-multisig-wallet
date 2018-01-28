dc4208a2228cc7bc3bf6d7b4fb6f3e301ee53dd5
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-01-28 13:53:27 -0500

    add comments about certain language feature deprecation

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -14,9 +14,9 @@ contract SimpleWallet {
 		owner = msg.sender; //setting the owner as the wallet creator so we know who created the contract (good practice in order to avoid mishaps like outsiders claiming ownership and doing something like deleting the contract)
 	}
 
-
 	//an anonymous function is called when the contract receives some funds or is called without any funds but without any function
 	//we look at whether the msg.sender is either the owner, or is allowed to deposit funds, and if so, we allow the tx to occur
+	//just found out this is deprecated. consider refactoring to something like revert() or require() in the main sendFunds function
 	function() {
 		if(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender] == true) {
 			Deposit(msg.sender, msg.value); //emitting an event
@@ -25,7 +25,7 @@ contract SimpleWallet {
 		}
 	}
 
-	// the sendFunds() function takes an amount, as well as the recipient address. Only those allowed to send funds can call this function. That is, if they are either the owner, or in our created mapping. It returns the balance at the end of the function.
+	// the sendFunds() function takes an amount, as well as the recipient address. Only those allowed to send funds can call this function. That is, if they are either the owner, or in our created mapping. It returns the balance at the end of the function. 
 	function sendFunds(uint amount, address beneficiary) returns (uint) {
 		if(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender]) {
 			if(this.balance >= amount) {
