a62009e6663a15cca6b9e5301ff6b5240e009827
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-01-28 13:42:49 -0500

    add sendFunds function and fallback function

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -14,6 +14,30 @@ contract SimpleWallet {
 		owner = msg.sender; //setting the owner as the wallet creator so we know who created the contract (good practice in order to avoid mishaps like outsiders claiming ownership and doing something like deleting the contract)
 	}
 
+
+	//an anonymous function is called when the contract receives some funds or is called without any funds but without any function
+	//we look at whether the msg.sender is either the owner, or is allowed to deposit funds, and if so, we allow the tx to occur
+	function() {
+		if(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender] == true) {
+			Deposit(msg.sender, msg.value); //emitting an event
+		} else {
+			throw;
+		}
+	}
+
+	// the sendFunds() function takes an amount, as well as the recipient address. Only those allowed to send funds can call this function. That is, if they are either the owner, or in our created mapping. It returns the balance at the end of the function.
+	function sendFunds(uint amount, address beneficiary) returns (uint) {
+		if(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender]) {
+			if(this.balance >= amount) {
+				if(!receiver.send(amount)) {
+					throw; //if for whatever reason, the receiver.send(amount) function does not work, we roll things back
+				}
+				Withdrawal(msg.sender, amount, beneficiary); //emitting an event
+				return this.balance;
+			}
+		}
+	}
+
 	// here we add new validated addresses to our mapping. This can once again only be called by the owner.
 	function allowAddressToSendMoney(address _address) {
 		if (msg.sender == owner) {
